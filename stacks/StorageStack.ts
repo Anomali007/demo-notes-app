155c132fb05824f08472bb2db9feb40694b38995
anomali007 <franzese007@gmail.com>
2024-03-09 22:40:54 -0600

    Enabling CORS

--- stacks/StorageStack.ts
+++ stacks/StorageStack.ts
@@ -3,7 +3,16 @@ import { StackContext, Table, Bucket } from "sst/constructs";
 export function StorageStack({ stack }: StackContext) {
   // Create S3 storage bucket
 
-  const bucket = new Bucket(stack, "Uploads");
+  const bucket = new Bucket(stack, "Uploads", {
+    cors: [
+      {
+        maxAge: "1 day",
+        allowedOrigins: ["*"],
+        allowedHeaders: ["*"],
+        allowedMethods: ["GET", "PUT", "POST", "DELETE", "HEAD"],
+      },
+    ],
+  });
 
   // create the dynamoDB table
   const table = new Table(stack, "Notes", {
